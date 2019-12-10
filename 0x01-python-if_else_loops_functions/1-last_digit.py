c3c900fe03ffacbf8c445ddbc6e9a8af3f86f6ff
paisap <santiagoaldana58@gmail.com>
2019-12-10 10:58:41 -0500

    all mandatories

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 == 0:
-    print('Last digit of {} is  0 and is 0'. format(number))
-elif number % 10 > 5:
-    print('Last digit of {} is {} and is greater than 5'.format(number, number%10))
+print("Last digit of {:d} is".format(number), end=" ")
+t = number
+if t < 0:
+    t = t * -1
+if t % 10 == 0:
+    if number < 0:
+        print('-', end="")
+    print('0 and is 0'.format(t % 10))
+elif t % 10 > 5:
+    if number < 0:
+        print('-', end="")
+    print('{} and is greater than 5'.format(t % 10))
 else:
-   print('Last digit of {} is {} and is less than 6 and not 0'.format(number, number%10))
+    if number < 0:
+        print('-', end="")
+    print('{} and is less than 6 and not 0'.format(t % 10))
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * insert_node - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * @number: the number
+ * Return: the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *aux = (*head);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	while (aux->next != NULL)
+	{
+		if (number > aux->n && number < aux->next->n)
+		{
+			new->next = aux->next;
+			aux->next = new;
+			break;
+		}
+		aux = aux->next;
+	}
+	return (new);
+}
