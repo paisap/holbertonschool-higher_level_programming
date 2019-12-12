39e2fbb7ee7851fb37d9df9053d9d9ae6ce47a6b
paisap <santiagoaldana58@gmail.com>
2019-12-12 12:23:47 -0500

    pruebas

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('0 arguments.')
+    else:
+        if len(sys.argv) == 2:
+            print('1 argument:')
+            print('1: {}'.format(sys.argv[1]))
+        else:
+            print('{} arguments:'.format(len(sys.argv)-1))
+            for i in range(1, len(sys.argv)):
+                print('{:d}: {}'.format(i, sys.argv[i]))
