4a2892e92ac29fb3daecef448653a44a0914932e
paisap <santiagoaldana58@gmail.com>
2019-12-12 12:39:03 -0500

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,7 @@ if __name__ == '__main__':
     import sys
     import hidden_4
     for i in dir(hidden_4):
-        if i[2:] != '__':
+        if i[:2] == '__':
+            continue
+        else:
             print(i)
