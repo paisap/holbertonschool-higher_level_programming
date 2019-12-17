cb99aeba1b3c6eaa125c4e8d6f5114c7f362b6e3
paisap <santiagoaldana58@gmail.com>
2019-12-17 12:39:14 -0500

    el null

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list)
-    if my_list:
+    if (my_list):
         for item in range(i-1, -1, -1):
             print('{:d}'.format(my_list[item]))
