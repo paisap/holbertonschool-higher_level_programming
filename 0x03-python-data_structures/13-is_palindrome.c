0b6b45e6335f825c31cb113b1864723b41e88f93
paisap <santiagoaldana58@gmail.com>
2019-12-17 17:08:34 -0500

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * is_palindrome - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: address of the new element or NULL if it fails
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *tmp1 = *head, *limit = *head, *tmp_prev;
+	int len = 0, i = 0;
+
+	while (limit->next != NULL)
+		len++, tmp_prev = limit, limit = limit->next;
+	len++;
+	while (i < (len / 2))
+	{
+		if (tmp1->n != limit->n)
+			return (0);
+		limit = tmp_prev;
+		tmp = tmp1;
+		while (tmp != limit)
+			tmp_prev = tmp, tmp = tmp->next;
+		tmp1 = tmp1->next;
+		i++;
+	}
+	return (1);
+}
