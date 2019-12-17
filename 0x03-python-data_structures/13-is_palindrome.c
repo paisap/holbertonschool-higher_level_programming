986bdebd0d1d6ae437dd147bf89300b6a39c7771
paisap <santiagoaldana58@gmail.com>
2019-12-17 17:53:30 -0500

    eficiencia

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -15,14 +15,12 @@ int is_palindrome(listint_t **head)
 	while (limit->next != NULL)
 		len++, tmp_prev = limit, limit = limit->next;
 	len++;
-	if (len == 1)
-		return (1);
 	while (i < (len / 2))
 	{
 		if (tmp1->n != limit->n)
 			return (0);
 		limit = tmp_prev;
-		tmp = tmp1;
+		tmp = tmp1->next;
 		while (tmp != limit)
 			tmp_prev = tmp, tmp = tmp->next;
 		tmp1 = tmp1->next;
