20b624ec296bb4b59d0ff24a4b072e8fbdd17c62
paisap <santiagoaldana58@gmail.com>
2019-12-17 14:28:00 -0500

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        j = 0
+        for i in my_list:
+            if i > j:
+                j = i;
+        return j
+    return "None"
