3961040bcae86b70ad0a893564a59fd67935f9e4
paisap <santiagoaldana58@gmail.com>
2019-12-19 15:51:56 -0500

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    matrix = [
+            ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+            [1, 5, 10, 50, 100, 500, 100]]
+    j = 0
+    l = 7
+    result = 0
+    for i in roman_string:
+        j = 0
+        for m in matrix[0]:
+            if i == m:
+                if l < j:
+                    result = matrix[1][j] - result
+                else:
+                    result += matrix[1][j]
+                l = j
+                j = 0
+            else:
+                j += 1
+    return result
