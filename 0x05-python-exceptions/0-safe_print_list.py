cf06c98fe6069693ea43947ca2dcfd5790c9edd6
paisap <santiagoaldana58@gmail.com>
2020-01-08 18:23:02 +0000

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,14 +4,13 @@ def safe_print_list(my_list=[], x=0):
     try:
         my_list[x - 1]
         for i in range(x):
-            print('{:d}'.format(my_list[i]), end='')
-        i += 1
+            print('{:d}'.format(my_list[i]), end="")
         print()
         return x
 
     except IndexError:
         for i in my_list:
-            print('{:d}'.format(i), end='')
+            print('{:d}'.format(i), end="")
             t += 1
         print()
         return t
