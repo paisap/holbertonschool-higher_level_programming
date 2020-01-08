ea79239351fa345edc74e31c83400381fd35e98d
paisap <santiagoaldana58@gmail.com>
2020-01-08 18:18:30 +0000

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,12 +6,12 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print('{:d}'.format(my_list[i]), end='')
         i += 1
-        print('')
-        return i
+        print()
+        return x
 
     except IndexError:
         for i in my_list:
             print('{:d}'.format(i), end='')
             t += 1
-        print("")
+        print()
         return t
