6e0d5166c97daacf5c436019a502e24415edd3d4
paisap <santiagoaldana58@gmail.com>
2020-01-09 22:45:52 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    """Exceptions are documented in the same way as classes.
+
+   The __init__ created a square.
+
+
+    Attributes:
+        size (int): Human readable string describing the exception.
+
+    """
+
+    def __init__(self, size=0):
+        """Example of square on the __init__ method.
+
+            Args:
+            int (int): is a size of square.
+        """
+        if type(size) is not int:
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
