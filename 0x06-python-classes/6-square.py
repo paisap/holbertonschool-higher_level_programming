a124ab53980da49f27e06429c2a05adddf661537
paisap <santiagoaldana58@gmail.com>
2020-01-10 01:11:05 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,9 +55,9 @@ class Square:
         else self.__position[1] > 0:
             for i in range(self.__position[1]):
                 print()
-        for i in range(self.__size):
-            for h in range(self.__position[0]):
-                print(" ", end='')
-            for j in range(self.__size):
-                print("#", end='')
-            print()
+            for i in range(self.__size):
+                for h in range(self.__position[0]):
+                    print(" ", end='')
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
