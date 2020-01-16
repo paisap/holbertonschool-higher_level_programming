55b35eed05b6d40a96001aab2c25d0172fe689cd
paisap <santiagoaldana58@gmail.com>
2020-01-16 05:08:14 +0000

    task all

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-n = 1
-
-
-def magic_string():
-    global n
-    hol = "Holberton, "
-    hol = hol * n
-    n += 1
-    return hol
+def magic_string(mylist=[]):
+    mylist.append("Holberton")
+    return ', '.join(mylist)
