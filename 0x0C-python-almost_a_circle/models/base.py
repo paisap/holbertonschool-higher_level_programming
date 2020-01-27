11d880c2fd28cb75da418200ad351ad1c66d48bb
paisap <santiagoaldana58@gmail.com>
2020-01-27 18:57:25 +0000

    <= 0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ ..."""
-        if len(list_dictionaries) <= 0 or list_dictionaries is None:
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
