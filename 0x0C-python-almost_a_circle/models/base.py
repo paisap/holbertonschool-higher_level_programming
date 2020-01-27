abb582b85a9bc0292053d0e375766426d4d97bb8
paisap <santiagoaldana58@gmail.com>
2020-01-27 05:46:33 +0000

    terminar todo mañana

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,21 +6,33 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        self.id = id
-
-    @property
-    def id(self):
-        return self.__id
-
-    @id.setter
-    def id(self, value=None):
-        if value is None:
+        if id is None:
             Base.__nb_objects += 1
-            self.__id = self.__nb_objects
+            self.id = Base.__nb_objects
         else:
-            self.__id = value
+            self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if len(list_dictionaries) <= 0 or list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = "" + cls.__name__ + ".json"
+        text = []
+        with open(filename, mode='w', encoding='UTF-8') as f:
+            if list_objs is None:
+                text = []
+            else:
+                for i in list_objs:
+                    text.append(i.to_dictionary())
+                json_list = cls.to_json_string(text)
+            f.write(json_list)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) <= 0:
+            return []
+        return json.loads(json_string)
