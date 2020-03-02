b42f6c5447982449ab8147191d57a7480340bc7c
paisap <santiagoaldana58@gmail.com>
2020-03-02 21:26:39 +0000

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- number by score
-SELECT score, count(score) as number from second_table GROUP BY score ORDER BY score DESC;
+SELECT score, count(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
