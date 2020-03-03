2147c6748192bbced9fd4d558ebc224d172ed2f4
paisap <santiagoaldana58@gmail.com>
2020-03-03 21:58:30 +0000

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- my first join
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.id=states.id ORDER BY id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.id=states.id ORDER BY id ASC;
