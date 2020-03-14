1006f5ef876ef3817878d16bd7b8e381fe9cc526
paisap <santiagoaldana58@gmail.com>
2020-03-14 21:56:05 +0000

    task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-
 def number_of_lines(filename=""):
-    j = 0
-    with open(filename, encoding='UTF8') as file:
-        for i in file:
-            j += 1
-    return j
+    x = 0
+    with open(filename, 'r') as f:
+        for i in f:
+            x += 1
+    return x
