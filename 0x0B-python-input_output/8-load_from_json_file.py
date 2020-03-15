2ce89f696ce533b873223eb6b91625de9a08515b
paisap <santiagoaldana58@gmail.com>
2020-03-15 23:36:32 +0000

    task 8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,8 +4,4 @@ import json
 
 def load_from_json_file(filename):
     with open(filename, 'r') as f:
-        try:
-            x = json.load(f)
-            return x
-        except Exception as f:
-            pass
+        return json.loads(f.read())
