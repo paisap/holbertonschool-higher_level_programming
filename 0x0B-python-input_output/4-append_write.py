a19c1c05bb943cefe471db817c8028d12c9f7530
paisap <santiagoaldana58@gmail.com>
2020-03-15 07:29:00 +0000

    task 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def append_write(filename="", text=""):
-    with open(filename, mode='a', encoding='UTF8') as file:
-        return file.write(text)
+    with open(filename, 'a', encoding='UTF-8')  as f:
+        return f.write(text)
--- 0x0B-python-input_output/7-save_to_json_file.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
