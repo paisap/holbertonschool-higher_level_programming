d5cacc5cf56f1c826ae79268560073cde1ab6243
paisap <santiagoaldana58@gmail.com>
2020-03-15 07:20:43 +0000

    task 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def write_file(filename="", text=""):
-    with open(filename, mode='w', encoding='UTF8') as file:
-        return file.write(text)
+    with open(filename, 'w') as f:
+        return f.write(text)
