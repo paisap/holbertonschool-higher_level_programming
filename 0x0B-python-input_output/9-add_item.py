6adc04a081683e869765c169f0f2fb304581c77d
paisap <santiagoaldana58@gmail.com>
2020-03-19 13:48:31 +0000

    Task

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 import sys
+
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-    x = []
-    y = []
-    f = "add_item.json"
-    for i in range(1, len(sys.argv)):
-        x.append(sys.argv[i])
-    try:
-        y = (load_from_json_file(f))
-    except Exception as t:
-        pass
-    for i in y:
-        x.append(i)
-    save_to_json_file(x, f)
+x = []
+y = []
+f = "add_item.json"
+for i in range(1, len(sys.argv)):
+    x.append(sys.argv[i])
+try:
+    y = (load_from_json_file(f))
+except Exception as t:
+    pass
+for i in y:
+    x.append(i)
+save_to_json_file(x, f)
