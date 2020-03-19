369951d0212687fc2a071a37c9edc9c91ba9e25b
paisap <santiagoaldana58@gmail.com>
2020-03-19 13:53:24 +0000

    Task

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 f = "add_item.json"
 try:
     y = load_from_json_file(f)
-except Exception :
+except Exception:
     y = []
 for i in range(1, len(sys.argv)):
     y.append(sys.argv[i])
