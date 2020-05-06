f6f77d1f9e22fdee1fe2b8a9ecd085f307ae9107
paisap <santiagoaldana58@gmail.com>
2020-05-06 10:15:18 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const matrix = [0];
+for (let i = 2; i < process.argv.length; i++) {
+  matrix[i - 2] = parseInt(process.argv[i]);
+}
+let mayor = matrix[0];
+let menor = mayor;
+for (let i = 0; i < matrix.length; i++) {
+  if (matrix[i] > mayor) {
+    menor = mayor;
+    mayor = matrix[i];
+  } else if (matrix[i] > menor && matrix[i] !== mayor) {
+    menor = matrix[i];
+  }
+}
+(process.argv.length === 3) ? console.log(0) : console.log(menor);
