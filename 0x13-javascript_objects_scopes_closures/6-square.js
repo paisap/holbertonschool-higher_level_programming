bca3097fb6bc328a5ce834e48b2f5284e0b4c3b2
paisap <santiagoaldana58@gmail.com>
2020-05-12 00:20:07 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,11 +7,11 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c || 'X');
-      }
-      console.log();
+      console.log(c.repeat(this.height));
     }
   }
 };
