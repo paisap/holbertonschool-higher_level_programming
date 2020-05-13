eaa3f1654f9d4d17d972c520e2b9f3977c543825
paisap <santiagoaldana58@gmail.com>
2020-05-13 19:44:02 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  console.log('code: ' + response.statusCode);
+  (error) ? console.log(error) : console.log('code: ' + response.statusCode);
 });
