07294aff5c306fe737cefd3e56dcf11c6e3a69cf
paisap <santiagoaldana58@gmail.com>
2020-05-13 18:58:25 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf8', function (error, data) {
+  if (error) { console.log(error); return; }
+  console.log(data);
+});
